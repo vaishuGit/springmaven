5ec7f2cc6390770b43f62e0c382b3e68498acd73
vaishuGit <70862985+vaishuGit@users.noreply.github.com>
2022-03-25 16:26:32 -0400

    Add files via upload

---
+++ url-checking/src/main/java/com/example/urlchecking/UrlCheckingApplication.java
@@ -0,0 +1,13 @@
+package com.example.urlchecking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlCheckingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlCheckingApplication.class, args);
+	}
+
+}
---
+++ url-checking/src/test/java/com/example/urlchecking/UrlCheckingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlchecking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlCheckingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
